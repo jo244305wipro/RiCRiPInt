ae6234db436bc4c1b625564309b8bda386386238
unknown <vimtk@L-156096827.wipro.com>
2014-07-30 16:31:02 +0530

    inside toolmap

--- SDK_framework/linux/toolmap.sh
+++ SDK_framework/linux/toolmap.sh
@@ -2,7 +2,7 @@
 # Define environment variables used by Harlequin Embedded SDK make files
 CV_DOXYGEN_1_6_2=/mnt/hgfs/test/RiCRiPInt/SDK_framework/linux/CV_variables/CV_DOXYGEN_1_6_2/doxygen_1.6.2 ; export CV_DOXYGEN_1_6_2
 CV_GCC_4_5_3=/mnt/hgfs/test/RiCRiPInt/SDK_framework/linux/CV_variables/CV_DOXYGEN_1_6_2/doxygen_1.6.2; export CV_GCC_4_5_3
-CV_GCC_4_5_3_CPLUSPLUS=/usr/bin/g++-4.5 ; export CV_GCC_4_5_3_CPLUSPLUS
+CV_GCC_4_5_3_CPLUSPLUS=/mnt/hgfs/test/RiCRiPInt/SDK_framework/linux/ ; export CV_GCC_4_5_3_CPLUSPLUS
 CV_GCC_4_5_3_CPP=/usr/bin/cpp-4.5 ; export CV_GCC_4_5_3_CPP
 CV_GRAPHVIZ_2=/mnt/hgfs/test/RiCRiPInt/SDK_framework/linux/CV_variables/CV_GRAPHVIZ_2 ; export CV_GRAPHVIZ_2
 CV_JAM_2_2_5_6=/mnt/hgfs/test/RiCRiPInt/SDK_framework/linux/CV_variables/CV_JAM_2_2_5_6/jam_2_2_5_6 ; export CV_JAM_2_2_5_6
